75685a9cb0f4f8f642a3507763d44f193fe07970
Runar Kristoffersen <git@runar.dev>
2022-03-05 13:02:39 +0100

    chore: change argon2-lib

--- localuser/pw.go
+++ localuser/pw.go
@@ -1,21 +1,21 @@
 package localuser
 
 import (
-	"github.com/tvdburgt/go-argon2"
+	"github.com/matthewhartstonge/argon2"
 )
 
 type PwHasher struct {
-	salt []byte
-	ctx  *argon2.Context
+	argon argon2.Config
 }
 
-func NewPwHasher(salt []byte) PwHasher {
-	return PwHasher{salt, argon2.NewContext()}
+func NewPwHasher(_salt []byte) PwHasher {
+	argon := argon2.DefaultConfig()
+	return PwHasher{argon}
 }
 
 func (p *PwHasher) Hash(pw string) ([]byte, error) {
-	return argon2.Hash(p.ctx, []byte(pw), p.salt)
+	return p.argon.HashEncoded([]byte(pw))
 }
 func (p *PwHasher) Verify(hash []byte, pw string) (bool, error) {
-	return argon2.Verify(p.ctx, hash, []byte(pw), p.salt)
+	return argon2.VerifyEncoded([]byte(pw), hash)
 }
