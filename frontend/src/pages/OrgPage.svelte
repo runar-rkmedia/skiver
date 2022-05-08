de2b180e6a0a12b8491db633e9d2b6a948439086
Runar Kristoffersen <gh@runar.dev>
2022-05-08 08:09:14 +0200

    stash

---
+++ frontend/src/pages/OrgPage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { db } from '../api'
+  export let organization: ApiDef.Organization
+</script>
+
+Org
+
+{#if !organization}
+  Not found
+{:else}
+  <pre>{JSON.stringify(organization, null, 2)}</pre>
+{/if}
